0a7477d66d031b769980bcf6254f3d6dea3106db
sakshi1305 <Sakshijain1305@gmail.com>
2020-07-21 13:17:18 +0530

    commit code

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,8 @@
+require 'navigation_helper.rb'
+
+module ApplicationHelper
+	include NavigationHelper
+	include PostsHelper
+	include Private::ConversationsHelper
+	include Private::MessagesHelper
+end
---
+++ config/routes.rb
@@ -0,0 +1,31 @@
+Rails.application.routes.draw do
+  get 'posts/index'
+  # devise_for :users
+  devise_for :users, :controllers => {:registrations => "registrations"}
+  devise_scope :user do
+  	get 'login', to: 'devise/sessions#new'
+  	get 'signup', to: 'devise/registrations#new'
+  end
+  root 'pages#index'
+
+  resources :posts do
+	  collection do
+	    get 'hobby'
+	    get 'study'
+	    get 'team'
+	  end
+  end
+
+  namespace :private do 
+    resources :conversations, only: [:create] do
+      member do
+        post :close
+        post :open
+      end
+    end
+    resources :messages, only: [:index, :create]
+  end
+
+  resources :contacts, only: [:create, :update, :destroy]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
