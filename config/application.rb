f3ec3c3a09cf889bdee7e72afdb3d67376d26052
SakshiNew <70377357+SakshiNew@users.noreply.github.com>
2020-08-29 16:07:25 +0530

    Update application.rb
    Add secret key.

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Collabfield
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.action_controller.include_all_helpers = false
+    config.secret_key_base = '154c77e2da58cc5d461322c990ab7c032a6464db8f2aac81fdfeb66425419b488bcf7d90b6edb9dcf88d99b9be990b3554a88329cad6ecbd0f0fd9615638449b'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
